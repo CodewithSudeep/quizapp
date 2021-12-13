e1e39a8db042d83deacb810cc1f6bc4813224fc2
SudeepMI <sudeepm0599@gmail.com>
2021-12-13 22:03:16 +0545

    all front end finished

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,15 +5,19 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
-                    </a>
+
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Home') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('addQuestion')" :active="request()->routeIs('addQuestion')">
+                        {{ __('Add Question') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('progress')" :active="request()->routeIs('progress')">
+                        {{ __('My Progress') }}
                     </x-nav-link>
                 </div>
             </div>
